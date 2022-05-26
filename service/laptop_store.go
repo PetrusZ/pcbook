1f7aff429517ffde8babbb96cd6fc781769c28f4
PetrusZ <silencly07@gmail.com>
2022-05-26 23:27:56 +0800

    feat: implement server-streaming gRPC API

--- service/laptop_store.go
+++ service/laptop_store.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
 	"sync"
 
 	"github.com/PetrusZ/pcbook/pb"
@@ -14,6 +16,7 @@ var ErrAlreadyExists = errors.New("record already exists")
 type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	Find(id string) (*pb.Laptop, error)
+	Search(ctx context.Context, filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 type InMemoryLaptopStore struct {
@@ -39,10 +42,9 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 	}
 
 	// deep copy
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
-		return fmt.Errorf("can't copy laptop data: %w", err)
+		return err
 	}
 
 	store.data[other.Id] = other
@@ -58,6 +60,85 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 		return nil, nil
 	}
 
+	return deepCopy(laptop)
+}
+
+func (store *InMemoryLaptopStore) Search(
+	ctx context.Context,
+	filter *pb.Filter,
+	found func(laptop *pb.Laptop) error,
+) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		// heavy processing
+		// time.Sleep(time.Second)
+		// log.Print("checking laptop id: ", laptop.Id)
+
+		if ctx.Err() == context.Canceled || ctx.Err() == context.DeadlineExceeded {
+			log.Print("context canceled or exceeded deadline")
+			return errors.New("context canceled or exceeded deadline")
+		}
+
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.MaxPriceUsd {
+		return false
+	}
+
+	if laptop.GetCpu().NumberCores < filter.MinCpuCores {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.MinCpuGhz {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.MinRam) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.Value
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 3 // 8 = 2 * 3
+	case pb.Memory_KILOBYTE:
+		return value << 13 // 1024 * 8 = 2^10 * 2^3 = 2^13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
 	err := copier.Copy(other, laptop)
 	if err != nil {
